b6f43096f67d06482df497fec21cc2751c134224
Blender3D <452469+Blender3D@users.noreply.github.com>
2014-05-09 07:34:11 -0400

    Revert "Restructured, added convenience classes"
    This reverts commit 6cb9b16d3fb4cfe90154a92c9044febe884b8535.

--- pygrooveshark/utils.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-import re
-
-def random_hex():
-    return ''.join(random.choice(string.hexdigits) for n in range(6))
-
-def windows_filename(filename):
-    return re.sub(r'[/\\:*?"<>|]', '', filename)
